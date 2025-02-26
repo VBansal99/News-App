721d2a85486ec47f5313bf4cc709eba1d1593b14
VBansal99 <varunbansaldcsamca@gmail.com>
2025-02-26 18:45:34 +0530

    Implemented Splash screen and Top App Bar

---
+++ app/src/main/java/com/floydwiz/newsapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.floydwiz.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.floydwiz.newsapp.ui.theme.NewsAppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        var keepSplashScreen = true
+        super.onCreate(savedInstanceState)
+        splashScreen.setKeepOnScreenCondition { keepSplashScreen }
+        lifecycleScope.launch {
+            delay(5000)
+            keepSplashScreen = false
+        }
+        enableEdgeToEdge()
+        setContent {
+            NewsAppTheme {
+                NewsTopAppBar()
+            }
+        }
+    }
+}
+
